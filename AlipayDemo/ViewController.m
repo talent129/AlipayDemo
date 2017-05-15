4859da8062445d73ee501b798168cd97efc00632
mac <mac@macdeMacBook-Pro.local>
2017-05-15 14:32:11 +0800

    简单集成支付宝Demo

---
+++ AlipayDemo/ViewController.m
@@ -0,0 +1,168 @@
+//
+//  ViewController.m
+//  AlipayDemo
+//
+//  Created by mac on 17/5/15.
+//  Copyright © 2017年 cai. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <AlipaySDK/AlipaySDK.h>
+#import "Order.h"
+#import "RSADataSigner.h"
+#import "APAuthV2Info.h"
+
+#define Screen_Width [UIScreen mainScreen].bounds.size.width
+#define Screen_Height [UIScreen mainScreen].bounds.size.height
+
+@interface ViewController ()
+
+@property (nonatomic, strong) UIButton *alipayBtn;
+
+@end
+
+@implementation ViewController
+
+#pragma mark -懒加载
+- (UIButton *)alipayBtn
+{
+    if (!_alipayBtn) {
+        _alipayBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        _alipayBtn.frame = CGRectMake(15, 100, Screen_Width - 30, 50);
+        _alipayBtn.backgroundColor = [UIColor cyanColor];
+        [_alipayBtn setTitle:@"支付宝" forState:UIControlStateNormal];
+        [_alipayBtn addTarget:self action:@selector(alipayBtnAction) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _alipayBtn;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor orangeColor];
+    
+    [self createUI];
+    
+}
+
+
+#pragma mark -createUI
+- (void)createUI
+{
+    [self.view addSubview:self.alipayBtn];
+    
+}
+
+- (void)alipayBtnAction
+{
+    //TIPS：这一步应在商户服务端完成，商户服务端直接将组装和签名后的请求串orderString传给客户端，客户端直接传给SDK发起请求。文档和Demo是为了示例效果在客户端实现。
+    
+    //重要说明
+    //这里只是为了方便直接向商户展示支付宝的整个支付流程；所以Demo中加签过程直接放在客户端完成；
+    //真实App里，privateKey等数据严禁放在客户端，加签过程务必要放在服务端完成；
+    //防止商户私密数据泄露，造成不必要的资金损失，及面临各种安全风险；
+    /*============================================================================*/
+    /*=======================需要填写商户app申请的===================================*/
+    /*============================================================================*/
+    NSString *appID = @"";
+    
+    // 如下私钥，rsa2PrivateKey 或者 rsaPrivateKey 只需要填入一个
+    // 如果商户两个都设置了，优先使用 rsa2PrivateKey
+    // rsa2PrivateKey 可以保证商户交易在更加安全的环境下进行，建议使用 rsa2PrivateKey
+    // 获取 rsa2PrivateKey，建议使用支付宝提供的公私钥生成工具生成，
+    // 工具地址：https://doc.open.alipay.com/docs/doc.htm?treeId=291&articleId=106097&docType=1
+    NSString *rsa2PrivateKey = @"";
+    NSString *rsaPrivateKey = @"";
+    /*============================================================================*/
+    /*============================================================================*/
+    /*============================================================================*/
+    
+    //partner和seller获取失败,提示
+    if ([appID length] == 0 ||
+        ([rsa2PrivateKey length] == 0 && [rsaPrivateKey length] == 0))
+    {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"提示"
+                                                        message:@"缺少appId或者私钥。"
+                                                       delegate:self
+                                              cancelButtonTitle:@"确定"
+                                              otherButtonTitles:nil];
+        [alert show];
+        return;
+    }
+    
+    /*
+     *生成订单信息及签名
+     */
+    //将商品信息赋予AlixPayOrder的成员变量
+    Order* order = [Order new];
+    
+    // NOTE: app_id设置
+    order.app_id = appID;
+    
+    // NOTE: 支付接口名称
+    order.method = @"alipay.trade.app.pay";
+    
+    // NOTE: 参数编码格式
+    order.charset = @"utf-8";
+    
+    // NOTE: 当前时间点
+    NSDateFormatter* formatter = [NSDateFormatter new];
+    [formatter setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
+    order.timestamp = [formatter stringFromDate:[NSDate date]];
+    
+    // NOTE: 支付版本
+    order.version = @"1.0";
+    
+    // NOTE: sign_type设置
+    order.sign_type = @"RSA";
+    
+    // NOTE: 商品数据
+    order.biz_content = [BizContent new];
+    order.biz_content.body = @"我是测试数据";
+    order.biz_content.subject = @"1";
+    order.biz_content.out_trade_no = @"将来由接口返回给你"; //订单ID（由商家自行制定）
+    order.biz_content.timeout_express = @"30m"; //超时时间设置
+    order.biz_content.total_amount = [NSString stringWithFormat:@"%.2f", 0.01]; //商品价格
+    
+    //将商品信息拼接成字符串
+    NSString *orderInfo = [order orderInfoEncoded:NO];
+    NSString *orderInfoEncoded = [order orderInfoEncoded:YES];
+    NSLog(@"orderSpec = %@",orderInfo);
+    
+    // NOTE: 获取私钥并将商户信息签名，外部商户的加签过程请务必放在服务端，防止公私钥数据泄露；
+    //       需要遵循RSA签名规范，并将签名字符串base64编码和UrlEncode
+//    id<DataSigner> signer = CreateRSADataSigner(privateKey);
+//    NSString *signedString = [signer signString:orderInfo];
+    
+    NSString *signedString = nil;
+    RSADataSigner* signer = [[RSADataSigner alloc] initWithPrivateKey:((rsa2PrivateKey.length > 1)?rsa2PrivateKey:rsaPrivateKey)];
+    if ((rsa2PrivateKey.length > 1)) {
+        signedString = [signer signString:orderInfo withRSA2:YES];
+    } else {
+        signedString = [signer signString:orderInfo withRSA2:NO];
+    }
+    
+    // NOTE: 如果加签成功，则继续执行支付
+    if (signedString != nil) {
+        //应用注册scheme,在AliSDKDemo-Info.plist定义URL types
+        NSString *appScheme = @"com.cai.alipayDemo";
+        
+        // NOTE: 将签名成功字符串格式化为订单字符串,请严格按照该格式
+        NSString *orderString = [NSString stringWithFormat:@"%@&sign=%@",
+                                 orderInfoEncoded, signedString];
+        
+        // NOTE: 调用支付结果开始支付
+        [[AlipaySDK defaultService] payOrder:orderString fromScheme:appScheme callback:^(NSDictionary *resultDic) {
+            NSLog(@"reslut = %@",resultDic);
+        }];
+    }
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # AlipayDemo
+
+# 支付宝开放平台: https://doc.open.alipay.com
